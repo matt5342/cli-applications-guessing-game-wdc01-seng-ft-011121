244944b766bb0191aef3870bade0abe71e6c6784
Matthew Sewell <mattfsewell@gmail.com>
2020-12-27 17:53:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,7 +10,7 @@ def run_guessing_game
   elsif input == "exit"
     puts "Goodbye!"
   else 
-    puts "Sorry! The computer guessed #{num}."
+    puts "/Sorry! The computer guessed #{num}./"
     0
   end
 
