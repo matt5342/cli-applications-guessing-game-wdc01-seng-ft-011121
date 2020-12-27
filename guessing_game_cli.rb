75a852df19cf095713f2d7759017e52a2e6899f8
Matthew Sewell <mattfsewell@gmail.com>
2020-12-27 18:56:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   
   if input == 'exit'
     puts "Goodbye!"
-  elsif input == num
+  elsif input.to_i == num
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{num}."
